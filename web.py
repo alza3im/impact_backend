33b5fd09ee5cf4e439e6ff509dad80d134a173a3
alza3im <khaleel_097@hotmail.com>
2021-05-04 03:29:56 -0300

    Fix break

--- web.py
+++ web.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return f"This is my library version {__version__}"
-    Break on purposes
+    
 @app.route("/fakedata")
 def fakedata():
     return json.dumps(fake_data())
